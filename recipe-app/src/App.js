a9e535772d4df4b3dde488cbdf6067c156181afb
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-19 18:27:35 +0100

    Create a account by API website and download id, key and example request and create variables

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const APP_ID = "6fda908f";
+  const APP_KEY = "cd9b902f74d0f14abc1c21fd9b85244f";
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello,World!</h1>
     </div>
   );
 }
