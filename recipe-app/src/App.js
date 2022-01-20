7a8c1b4cb7fe828c88c2fe238aff8a7baaa9949b
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-20 20:27:05 +0100

    Add searching new results

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -7,25 +7,41 @@ function App() {
   const APP_KEY = "cd9b902f74d0f14abc1c21fd9b85244f";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
 
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    //console.log(data.hits);
+  };
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+    //console.log(search);
+  };
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
   };
 
   return (
     <div className='App'>
-      <form className='search-form'>
-        <input className='search=bar' type='text' />
+      <form onSubmit={getSearch} className='search-form'>
+        <input
+          className='search=bar'
+          type='text'
+          value={search}
+          onChange={updateSearch}
+        />
         <button className='search-button' type='submit'>
           Search
         </button>
