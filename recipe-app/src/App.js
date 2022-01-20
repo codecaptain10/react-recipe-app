04e727a5e54b0ea1a78c04c9040889b50611c5e6
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-20 18:47:08 +0100

    Use API data to get title, calories and image

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -31,7 +31,11 @@ function App() {
         </button>
       </form>
       {recipes.map(recipe => (
-        <Recipe />
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
       ))}
     </div>
   );
